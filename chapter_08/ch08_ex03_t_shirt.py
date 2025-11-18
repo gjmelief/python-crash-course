84f1ae09fe9c9e455073c688b9a67c44ac434226
gjmelief <gjmelief@gmail.com>
2025-11-18 11:26:52 +0100

    Exercise 8-3. Function call with posistional arguments

---
+++ chapter_08/ch08_ex03_t_shirt.py
@@ -0,0 +1,12 @@
+# Exercise 8-2 From 'Python Crash Course'
+# 18-11-2025 G. Melief
+
+# Exercise defining a function with parameters
+
+def make_shirt(size, text):
+    """Display a message with the size and the text on the shirt"""
+    print(f"You ordered a T-Shirt in the size {size.upper()} and the print "
+          f"{text.title()}")
+
+# Call the function with positional arguments
+make_shirt("xl", "i love python")
