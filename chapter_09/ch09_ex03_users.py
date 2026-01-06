e635ff9e1d65dbfd3209092f586a63378ed147d8
gjmelief <gjmelief@gmail.com>
2026-01-06 12:33:15 +0100

    Fixed identation error. Added greet_user method. Created the user# instances. Called both method's from the User class

--- chapter_09/ch09_ex03_users.py
+++ chapter_09/ch09_ex03_users.py
@@ -7,15 +7,33 @@ class User:
     '''A class describing information about a user'''
 
 
-def __init__(self, first_name, last_name, age, location):
-    '''Initialize attributes to describe a user'''
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
-    self.location = location
+    def __init__(self, first_name, last_name, age, location):
+        '''Initialize attributes to describe a user'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.location = location
 
-def describe_user(self):
-    '''Method for printing information about a user'''
-    print(f"The user's name is {self.first_name.capitalize()}"
-          f"{self.last_name.capitalize()}. The user is {self.age} year's old"
-          f"and lives in {self.location.capitalize()}")
+    def describe_user(self):
+        '''Method for printing information about a user'''
+        print(f"\nThe user's name is {self.first_name.capitalize()} "
+            f"{self.last_name.capitalize()}. The user is {self.age} year's old "
+            f"and lives in {self.location.title()}")
+
+    def greet_user(self):
+        '''Method for printing a personalized greeting to the user'''
+        print(f"\nHello {self.first_name.capitalize()} {self.last_name.capitalize()}. "
+              f"How is the weather in {self.location.title()}?")
+
+# Create several instances representing different users
+user0 = User('gert-jan', 'melief', 37, 'the netherlands')
+user1 = User('henk', 'smit', 42, 'germany')
+user2= User('john', 'doe', 54, 'the united kingdom')
+
+# Call both method from the Users class for different users
+user0.describe_user()
+user0.greet_user()
+user1.describe_user()
+user1.greet_user()
+user2.describe_user()
+user2.greet_user()
