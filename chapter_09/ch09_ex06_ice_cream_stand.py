8a6633f176dda7716aba13a69e6beca519c85bf5
gjmelief <gjmelief@gmail.com>
2026-01-08 06:09:00 +0100

    First part of Exercise 9-5. Created the subclass IceCreamStand and run a test

---
+++ chapter_09/ch09_ex06_ice_cream_stand.py
@@ -0,0 +1,48 @@
+# Exercise 9-6 From 'Python Crash Course'
+# 07-01-2026 G. Melief
+
+# Exercise creating a subclass. Using the class from exercise 9-4
+
+class Restaurant:
+    '''A class describing information about a restaurant'''
+
+
+    def __init__(self, restaurant_name, cuisine_type):
+        '''Initialize attributes to describe a restaurant'''
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        '''Display the attributes of the restaurant'''
+        print(f"\nThe name of the restaurant is {self.restaurant_name.capitalize()}."
+                f"\nThey are serving {self.cuisine_type} food.")
+
+    def open_restaurant(self):
+        '''Display a message stating the restaurant is open'''
+        print(f"\n{self.restaurant_name.capitalize()} is open")
+
+    def set_number_served(self, served):
+        '''Method for updating the number_served value'''
+        if served >= self.number_served:
+            self.number_served = served
+            print(f"Customers served: {self.number_served}")
+        else:
+            print("You can't 'unserve' customers!")
+
+    def increment_number_served(self, served):
+        '''Method for updating the number_served value with increment'''
+        self.number_served += served
+
+class IceCreamStand(Restaurant):
+    '''
+    A subclass from the Restaurant Class. Describing a specific kind of restaurant
+    '''
+
+
+    def __init__(self, restaurant_name, cuisine_type='Ice Cream Stand'):
+        '''Initialize the attributes of the superclass'''
+        super().__init__(restaurant_name, cuisine_type)
+
+gelato = IceCreamStand('Gelato')
+gelato.describe_restaurant()
