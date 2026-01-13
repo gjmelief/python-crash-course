a0a54708d812f48841d722ca9672f83260ee5712
gjmelief <gjmelief@gmail.com>
2026-01-13 13:15:59 +0100

    Updated my_cars.py and my_electric_car.py to use aliases when importing modules and classes

--- chapter_09/my_electric_car.py
+++ chapter_09/my_electric_car.py
@@ -1,6 +1,6 @@
-from car import ElectricCar
+import electric_car as ec
 
-my_leaf = ElectricCar('nissan', 'leaf', 2024)
+my_leaf = ec.ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.battery.describe_battery()
 my_leaf.battery.get_range()
