badd8e0768635dde88b15f313b0fe4f622087629
gjmelief <gjmelief@gmail.com>
2026-01-21 19:46:29 +0100

    Executed 10-3. Simplefied the code in the practice programs

---
+++ chapter_10/ch10_ex03_simpler_code.py
@@ -0,0 +1,14 @@
+# Exercise 10-2 From 'Python Crash Course'
+# 21-01-2026 G. Melief
+
+# Exercise making code simpler
+
+from pathlib import Path
+
+path = Path('chapter_10/pi_digits.txt')
+# contents = path.read_text().rstrip()
+# print(contents)
+
+contents = path.read_text()
+for line in contents.splitlines():
+    print(line)
