e18a39a0ee905ce1c9c865b6a357527723c11dbe
gjmelief <gjmelief@gmail.com>
2026-01-22 20:00:12 +0100

    See commit about 10-6

---
+++ chapter_10/ch10_ex07_addition_calculator.py
@@ -0,0 +1,21 @@
+# Exercise 10-6 From 'Python Crash Course'
+# 22-01-2026 G. Melief
+
+# Exercise using a try-except block and a while loop
+
+print("Give me two numbers, and I will add them together.")
+print("Press 'q' to quit")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) + int(second_number)
+    except ValueError:
+        print("You can not input text")
+    else:
+        print(answer)
